47fc92c76a2f5c6cb4d6e611d2f37c1a0f7a017c
Prit Savaliya <preetsavaliya077@gmail.com>
2026-08-24 22:47:49 +0530

    Time: 0 ms (100%), Space: 8.4 MB (98.18%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,19 @@
+#include <limits.h> // For INT_MAX and INT_MIN
+
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            int digit = x % 10;
+
+            // Check for overflow before multiplying
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > 7)) return 0;
+            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && digit < -8)) return 0;
+
+            ans = ans * 10 + digit;
+            x /= 10;
+        }
+        return ans;
+    }
+};
